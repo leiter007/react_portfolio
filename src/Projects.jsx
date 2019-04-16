34be466efd0b37b343bbbfc2892bd6ea51fb1ddf
felix_bonnier <felix_bonnier@yahoo.com>
2019-04-16 08:30:00 +0200

    changing projects function into a stateful class in Projects.jsx, and assigns first project information

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,43 @@
-import React from "react"
+import React, {Component} from "react"
 
-const Projects = () => {
-    return (
-        <div>
-            <h1 className="content-h1">Projects</h1>
-            <p className="content-text">Ipsum dolerum PROJECTS whatever</p>
-        </div>
-    )
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [
+                {
+                    "id": 1,
+                    "name": "My First Website"
+                },
+                {
+                    "id": 2,
+                    "name": "FizzBuzz"
+                }
+            ]
+        };
+    }
+    render() {
+        const projects = this.state.projects
+        let projectsList
+
+        if (projects.length >0) {
+            projectsList = projects.map(project => {
+                return (
+                    <div key={project.id}>
+                        <h3 className="content-text">
+                        {projects.name}
+                        </h3>
+                    </div>
+                )
+            })
+        }
+        return (
+            <div>
+                <h1 className="content-h1">My Projects</h1>
+                {projectsList}
+            </div>
+        )
+    }
 };
 
 export default Projects
