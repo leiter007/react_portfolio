0d5c230ebeb922b28db93f959f8bea78710092cd
felix_bonnier <felix_bonnier@yahoo.com>
2019-04-16 19:29:13 +0200

    adds styling to project card button

--- src/About.jsx
+++ src/About.jsx
@@ -4,12 +4,12 @@ import { UndrawDesigner } from "react-undraw-illustrations";
 const About = () => {
     return (
         <div className="content-wrapper">
-            <div className="flex flex-wrap mb-4">
-                <div className="w-full lg:w-1/4 md:w-1/2">
-                <UndrawDesigner primaryColor='#20504f' skinColor="#f3d3cd" hairColor="#b29a5e" height='200px'/>
+            <div className="flex flex-wrap mb-10 border-b border-grey-light">
+                <div className="w-full lg:w-1/6 md:w-1/2 -mr-2 pb-3">
+                <UndrawDesigner primaryColor='#20504f' skinColor="#f3d3cd" hairColor="#b29a5e" height='70px'/>
                 </div>
             
-                <div className="w-full lg:w-3/4 md:w-1/2">
+                <div className="w-full lg:w-5/6 md:w-1/2">
                 <h1 className="content-h1">About Me</h1>
                 <p className="content-text">Ipsum dolerum whatever</p>
                 </div>
--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -7,14 +7,14 @@ const ProjectCard = (props) => {
             <img src={project.image} className="block h-auto w-full" style={{ height: '150px', objectFit: 'cover' }} />
 
             <div className="px-6 py-4" style={{ minHeight: '150px' }}>
-                <div className="font-bold text-xl mb-2">{project.name}</div>
-                <p className="text-grey-darker text-base">
+                <div className="card-headline">{project.name}</div>
+                <p className="card-text">
                     {project.description}
                 </p>
             </div>
 
             <div className="flex items-center justify-between leading-none p-2 md:p-4">
-                <button className="bg-blue-darkest hover:bg-blue-dark text-white text-xs py-1 px-2 rounded-full">
+                <button className="card-button hover">
                     Details
                 </button>
             </div>
